b552912432c5a987a5c2a30ed4c1dee21a756a0e
J-Amruthasai <105196917+J-Amruthasai@users.noreply.github.com>
2025-04-16 12:36:01 +0530

    Update invoice_tasks.py

--- background_worker/invoice_tasks.py
+++ background_worker/invoice_tasks.py
@@ -3,23 +3,15 @@ from azure.storage.blob import BlobServiceClient
 import zipfile
 import io
 import requests
-from bson import ObjectId
 from config.config import collection
 from config.blob import AZURE_CONNECTION_STRING, AZURE_CONTAINER_NAME
-from Invoice_processing.invoice_reader import analyze_invoice
+# from Invoice_processing.invoice_reader import analyze_invoice
+from src.utils import convert_objectid, ObjectId, extract_zip_files
 
 celery_app = Celery("invoice_tasks", broker="redis://redis:6379/0")
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
 container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
-def convert_objectid(obj):
-    if isinstance(obj, ObjectId):
-        return str(obj)
-    elif isinstance(obj, dict):
-        return {k: convert_objectid(v) for k, v in obj.items()}
-    elif isinstance(obj, list):
-        return [convert_objectid(item) for item in obj]
-    else:
-        return obj
+
 
 def upload_bytes_to_blob(file_bytes, blob_name):
     blob_client = container_client.get_blob_client(blob_name)
@@ -33,36 +25,36 @@ def process_invoice_files(process_id, filename, file_bytes, app_key, is_zip,
     try:
         if is_zip:
             zip_folder = filename.rsplit(".", 1)[0]
-            with zipfile.ZipFile(io.BytesIO(file_bytes), 'r') as zip_ref:
-                extracted_files = [f for f in zip_ref.namelist() if not f.startswith("__MACOSX") and not f.endswith("/")]
-                for extracted_file in extracted_files:
-                    extracted_bytes = zip_ref.read(extracted_file)
-                    blob_name = f"{promo_event_name}/{zip_folder}/{extracted_file}"
-                    blob_url = upload_bytes_to_blob(extracted_bytes, blob_name)
-                    try:
-                        data = analyze_invoice(blob_url)
-                        # data = {}
-                        data["_id"] = str(ObjectId())
-                        data["process_id"] = process_id
-                        data["filename"] = extracted_file
-                        data["blob_url"] = blob_url
-                        data["app_key"] = app_key
-                        data["status"] = "processing"
-                        data["promo_event_name"] = promo_event_name
-                        data["start_date"] = start_date
-                        data["end_date"] = end_date
-                        data["locale"] = locale
-                        cleaned_data = convert_objectid(data)
-                        collection.insert_one(cleaned_data)
-                        response_data[extracted_file] = cleaned_data
-                    except Exception as e:
-                        response_data[extracted_file] = {"error": str(e)}
+            extracted_files_data = extract_zip_files(file_bytes)
+
+            for extracted_file, extracted_bytes in extracted_files_data:
+                blob_name = f"{promo_event_name}/{zip_folder}/{extracted_file}"
+                blob_url = upload_bytes_to_blob(extracted_bytes, blob_name)
+
+                try:
+                    # data = analyze_invoice(blob_url)
+                    data = {}
+                    data["_id"] = str(ObjectId())
+                    data["process_id"] = process_id
+                    data["filename"] = extracted_file
+                    data["blob_url"] = blob_url
+                    data["app_key"] = app_key
+                    data["status"] = "processing"
+                    data["promo_event_name"] = promo_event_name
+                    data["start_date"] = start_date
+                    data["end_date"] = end_date
+                    data["locale"] = locale
+                    cleaned_data = convert_objectid(data)
+                    collection.insert_one(cleaned_data)
+                    response_data[extracted_file] = cleaned_data
+                except Exception as e:
+                    response_data[extracted_file] = {"error": str(e)}
         else:
             blob_name = f"{promo_event_name}/invoices/{filename}"
             blob_url = upload_bytes_to_blob(file_bytes, blob_name)
             try:
-                data = analyze_invoice(blob_url)
-                # data = {}
+                # data = analyze_invoice(blob_url)
+                data = {}
                 data["_id"] = str(ObjectId())
                 data["process_id"] = process_id
                 data["filename"] = filename
@@ -75,7 +67,7 @@ def process_invoice_files(process_id, filename, file_bytes, app_key, is_zip,
                 data["locale"] = locale
                 cleaned_data = convert_objectid(data)
                 collection.insert_one(cleaned_data)
-                response_data[extracted_file] = cleaned_data
+                response_data[filename] = cleaned_data
             except Exception as e:
                 response_data[filename] = {"error": str(e)}
 
