07484d20fec5762405b41a5b82ed611dfc47b8c6
J-Amruthasai <105196917+J-Amruthasai@users.noreply.github.com>
2025-04-16 14:19:29 +0530

    Update invoice_tasks.py

--- background_worker/invoice_tasks.py
+++ background_worker/invoice_tasks.py
@@ -1,22 +1,10 @@
 from celery import Celery
-from azure.storage.blob import BlobServiceClient
-import zipfile
-import io
 import requests
-from config.config import collection
-from config.blob import AZURE_CONNECTION_STRING, AZURE_CONTAINER_NAME
-# from Invoice_processing.invoice_reader import analyze_invoice
+from src.config import collection
 from src.utils import convert_objectid, ObjectId, extract_zip_files
+from services.azure_service import upload_bytes_to_blob
 
 celery_app = Celery("invoice_tasks", broker="redis://redis:6379/0")
-blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
-container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
-
-
-def upload_bytes_to_blob(file_bytes, blob_name):
-    blob_client = container_client.get_blob_client(blob_name)
-    blob_client.upload_blob(file_bytes, overwrite=True)
-    return blob_client.url
 
 @celery_app.task
 def process_invoice_files(process_id, filename, file_bytes, app_key, is_zip,
