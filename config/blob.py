9c1381c6d65111e1d5ddb63d66191b60d8ef9a26
J-Amruthasai <105196917+J-Amruthasai@users.noreply.github.com>
2025-04-16 14:42:31 +0530

    Update blob.py

--- config/blob.py
+++ config/blob.py
@@ -4,12 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+# Azure credentials 
+endpoint = os.getenv("AZURE_FORM_RECOGNIZER_ENDPOINT")
+key = os.getenv("AZURE_FORM_RECOGNIZER_KEY")
+
 # Use the copied connection string here
 AZURE_CONNECTION_STRING =os.getenv("AZURE_CONNECTION_STRING")
 AZURE_CONTAINER_NAME =os.getenv("CONTAINER_NAME")
 
-# Initialize the BlobServiceClient
-blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
+# # Initialize the BlobServiceClient
+# blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
 
-# Example of interacting with Blob Storage
-container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
+# # Example of interacting with Blob Storage
+# container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
